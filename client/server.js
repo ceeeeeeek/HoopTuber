8950f1d023e160c7119c349c6d1bb03a92fc00d9
chris <bbhriss@gmail.com>
2025-10-03 22:46:27 -0700

    New

--- client/server.js
+++ client/server.js
@@ -75,8 +75,7 @@ async function start() {
       cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-
-        sameSite: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 1000 * 60 * 60 * 24,
       },
     })
