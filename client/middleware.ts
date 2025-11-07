8657c32c252dc1236795faec085f3e3f4b7af6e6
chris <bbhriss@gmail.com>
2025-11-06 17:01:41 -0800

    UPDATED

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,4 +1,21 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server";
+
+
+export default withAuth(
+  function middleware(req) {
+    const url = req.nextUrl.pathname;
+    if (url.endsWith(".php")) {
+      return new NextResponse("Not Found", {status: 404});
+    }
+    return NextResponse.next();
+  },
+  {
+    pages: {
+      signIn: "/auth/signin",
+    },
+  }
+);
 
 export const config = {
   matcher: [
