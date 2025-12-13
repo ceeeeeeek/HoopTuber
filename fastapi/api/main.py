307baa6c62916e1815b81553b2b7475f29f355b1
chris <bbhriss@gmail.com>
2025-12-12 23:15:20 -0800

    fastapi7

--- fastapi/api/main.py
+++ fastapi/api/main.py
@@ -71,8 +71,8 @@ if os.getenv("ENVIRONMENT") != "production":
 app.add_middleware(
     CORSMiddleware,
     #allow_origins=['http://localhost:3000'],
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=origins2,
+    allow_credentials=False,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allow_headers=["*"],
     expose_headers=["*"],
